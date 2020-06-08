f5536c8c2ccbea5292344e52f233a2f5b48818ec
cedrichol <65404079+cedrichol@users.noreply.github.com>
2020-06-08 16:06:38 +0200

    Update solver_2226.py
    c'etait faux désolé

--- kwyk-solver/solvers/solver_2226.py
+++ kwyk-solver/solvers/solver_2226.py
@@ -1,13 +1,21 @@
 import normale
-from functions import float_input
+from functions import float_input, int_input
 
 def solve():
     print("X suit la loi normale N(u,o) :")
     u = float_input("u = ")
     o = float_input("o = ")
     norm = normale.normale(u,o)
-    print("La question est P(X <= ?)")
-    a = float_input("? = ")
-    p = round(norm.p_X_inf(a),4)
+    print("La question est :")
+    print("1. P(X <= a)")
+    print("2. P(X >= a)")
+
+    choix=0
+    while choix is not in [1,2] : choix = int_input("1/2 :")
+    a = float_input("a = ")
+    
+    if choix == 1 : p = round(norm.p_X_inf(a),4)
+    if choix == 2 : p = round(norm.p_X_sup(a),4)
+
     print(f"\nP(X <= {a}) = {p}")
 
