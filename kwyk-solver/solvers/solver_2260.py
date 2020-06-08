0ac7660ef1e1b626e8be0b1d330811c2cd7eaeb7
cedrichol <65404079+cedrichol@users.noreply.github.com>
2020-06-08 16:14:19 +0200

    j'ai encore fait nimp avec le 2260

--- kwyk-solver/solvers/solver_2260.py
+++ kwyk-solver/solvers/solver_2260.py
@@ -3,16 +3,16 @@ from functions import float_input
 
 def solve():
     print("X suit la loi normale N(u,o) :")
-    u = float_input("u = ")
+    v = float_input("u = ")
     o = float_input("o = ")
-    norm = normale.normale(u,o)
+    norm = normale.normale(v,o)
     print("La question est P(3-u < X < 3+u) = a")
     a = float_input("a = ")
 
     u = 0.5001
     #recherche bourrin à 10^-3 pres, la dichotomie c'est nul anyway
-    while norm.p_X_btw(3-u,3+u) < a : u += 0.001 #pousser la limite quand la proba est trop petite
-    while norm.p_X_btw(3-u,3+u) > a : u -= 0.001 #idem quand trop grande
+    while norm.p_X_btw(v-u,v+u) < a : u += 0.001 #pousser la limite quand la proba est trop petite
+    while norm.p_X_btw(v-u,v+u) > a : u -= 0.001 #idem quand trop grande
 
     u = round(u,2)
     print(f"\nu = {u}")
