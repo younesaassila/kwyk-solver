2e35dea236c992095edf46afca023921de49ed11
younesaassila <47226184+younesaassila@users.noreply.github.com>
2023-08-14 20:54:18 +0200

    Déplacement du site web vers la branche "gh-pages"

--- docs/app.js
+++
@@ -1,7 +0,0 @@
-(async function () {
-  // Fetch download link of the latest version.
-  const { url: downloadLink } = await fetch("update.json")
-    .then((response) => response.json())
-    .catch(console.error);
-  document.querySelector("#download-link").setAttribute("href", downloadLink);
-})();
